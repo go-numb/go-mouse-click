70ee774085f407d51be08bb6b17225f0b8c95f0c
go-numb66 <gonumb66@gmail.com>
2023-04-05 00:55:44 +0900

    get mouse xy for drug

--- get.go
+++ get.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/go-vgo/robotgo"
 	hook "github.com/robotn/gohook"
 	"gonum.org/v1/gonum/stat"
 )
@@ -114,4 +115,57 @@ L:
 	}
 
 	return
+}
+
+type Corners struct {
+	X1 int
+	Y1 int
+
+	X2 int
+	Y2 int
+}
+
+// GetFourCorners クリック座標を指定回数分取得し返す
+func GetFourCorners() *Corners {
+	evChan := hook.Start()
+	hook.Register(hook.MouseDown, []string{"w"}, func(e hook.Event) {
+		fmt.Println("click")
+	})
+	defer hook.End()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	fmt.Println(STARTWORD)
+
+	c := new(Corners)
+
+L:
+	for ev := range evChan {
+		select {
+		case <-ctx.Done():
+			break L
+
+		default:
+			if ev.Kind == hook.MouseHold {
+				c.X1, c.Y1 = robotgo.GetMousePos()
+			} else if ev.Kind == hook.MouseDown {
+				c.X2, c.Y2 = robotgo.GetMousePos()
+			}
+
+			if c._done() {
+				break L
+			}
+		}
+	}
+
+	return c
+}
+
+func (c *Corners) _done() bool {
+	if c.X1 != 0 && c.Y1 != 0 && c.X2 != 0 && c.Y2 != 0 {
+		return true
+	}
+
+	return false
 }
